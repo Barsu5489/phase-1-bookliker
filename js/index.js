2823397596b76756ecde72c92eccc637317469a7
barsu5489 <ebarsulai@gmail.com>
2022-12-24 01:35:31 +0300

    debugging  patch fecth

--- js/index.js
+++ js/index.js
@@ -19,7 +19,9 @@ function display(){
 
 }
 
-
+function update(id){
+   
+}
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -34,8 +36,11 @@ document.addEventListener("DOMContentLoaded", function() {
             list.appendChild(li) 
             li.textContent = data.title
             const titles = li.textContent
+           
             console.log(titles)
+            const btn = document.createElement('button')
             li.addEventListener('click', ()=>{
+                 const idN = data.id
                 console.log(data.users)
                 const h3 = document.createElement('h3');
                 h3.textContent =data.subtitle
@@ -43,24 +48,45 @@ document.addEventListener("DOMContentLoaded", function() {
                 p.textContent = data.description
                 const ul = document.createElement('ul')
                 const li2 = document.createElement('li')
+                
+                btn.textContent = 'like'
                 ul.appendChild(li2)
                 data.users.forEach(user=>{
                     console.log(user.username)
                     ul.innerHTML += `<li>${user.username}</li>`
                 })
-             if (divData.innerHTML === ''){
+             if (divData.innerHTML === '')
+             {
                 divData.appendChild(h3)
                 divData.appendChild(p)
                 divData.appendChild(ul)
+                divData.appendChild(btn)
              }else {
                 divData.innerHTML = '';
                 divData.appendChild(h3)
                 divData.appendChild(p)
                 divData.appendChild(ul)
+                divData.appendChild(btn)
              }
-            
-                
+          
+               
 
+            })
+            btn.addEventListener('click', ()=>{
+                console.log('CLICKED')
+                fetch(`http://localhost:3000/books/:${data.id}`, {
+                    method: 'PATCH',
+                    body: JSON.stringify({
+                        id:1,
+                        username:"pouros"
+                    }),
+                    headers:{
+                        'Content-type': 'application/json; charset=UTF-8',
+                        'Accept': 'application/json',
+                    }
+                })
+                .then(res=>res.json())
+                .then(data=>console.log(data))
             })
         })
     
